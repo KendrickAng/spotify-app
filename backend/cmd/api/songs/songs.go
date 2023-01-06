b30b08f0beeb94ea75ca2857ab20fe7bf7d88d5b
Kendrick Ang <kendrick.wh@outlook.com>
2023-01-06 13:18:08 +0800

    integrate song generator and spotify api

--- backend/cmd/api/songs/songs.go
+++ backend/cmd/api/songs/songs.go
@@ -3,12 +3,24 @@ package songs
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
+	"github.com/KendrickAng/spotify-app/internal/random"
 	"github.com/KendrickAng/spotify-app/internal/spotify"
 )
 
 func GenerateHandler(s spotify.Spotify) func(http.ResponseWriter, *http.Request) {
+	// generate the search query params
+	query := fmt.Sprintf("*%s*", random.RandomAlphabet())
+	types := []string{"track"}
+	limit := 10
+	offset := random.RandomInt(spotify.SearchMinOffset, spotify.SearchMaxOffset)
+
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "songs works")
+		tracks, err := s.Search(query, types, limit, offset)
+		if err != nil {
+			fmt.Fprintln(w, err)
+		}
+		fmt.Fprintf(w, "songs works" + strconv.Itoa(len(tracks.Items)))
 	}
 }
