61f91992239bc177d55877cb2428f129c0ce8b75
Kendrick Ang <kendrick.wh@outlook.com>
2023-01-10 23:27:18 +0800

    add song generation refreshing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,44 @@
-import type { Component } from "solid-js";
-import { createSignal, createResource } from "solid-js";
+import type { Resource } from "solid-js";
+import { Component, Match, Show, createSignal, createResource, Switch, For } from "solid-js";
+import internal from "stream";
 
 import styles from "./App.module.css";
 
 const GITHUB_LINK = "https://github.com/KendrickAng/spotify-app";
+
+type SongsResponse = {
+  error: string;
+  tracks: Track[];
+}
+
+type Track = {
+  id: string;
+  name: string;
+  uri: string;
+  external_url: string;
+  album: Album;
+}
+
+type Album = {
+  id: string;
+  name: string;
+  uri: string;
+  external_url: string;
+  images: Image[];
+}
+
+type Image = {
+  height: number;
+  width: number;
+  url: string;
+}
 
 const fetchSongs = async () => {
   const url = import.meta.env.VITE_SERVER_URL + "/songs/generate";
   const res = await fetch(url, {
     method: "GET",
   });
-  const ret = await res.json();
-  console.log(ret);
-  return ret;
+  return await res.json();
 };
 
 const fetchHealth = async () => {
@@ -24,26 +50,41 @@ const fetchHealth = async () => {
 };
 
 const App: Component = () => {
-  const [songs, { mutate, refetch }] = createResource(fetchSongs);
+  const [songs, { refetch }] = createResource(fetchSongs);
   const [health] = createResource(fetchHealth);
-  console.log(JSON.stringify(songs()));
-  console.log("Loading: " + songs.loading);
-  console.log("Error: " + songs.error);
 
   return (
     <div>
+      <div>{"Health: " + health()}</div>
       <h1>spotify</h1>
       <p>song picker</p>
       <div>
-        <a>generate</a>
+        <button onClick={() => refetch()}>generate</button>
       </div>
       <div>
         <a href={GITHUB_LINK}>github</a>
       </div>
-      <div>{"Loading: " + songs.loading}</div>
-      <div>{"Error: " + songs.error + "\n"}</div>
-      <div>{JSON.stringify(songs(), null, 2)}</div>
-      <div>{"Health: " + health()}</div>
+      <Switch fallback={<p>Click "Generate" to find songs!</p>}>
+        <Match when={songs && songs.loading}>
+          Loading...
+        </Match>
+        <Match when={songs && songs.error}>
+          <div>Sorry, we met an issue fetching songs... please try again later!</div>
+          <div>The following error occured: {songs.error}</div>
+        </Match>
+        <Match when={songs && songs()}>
+          <ol>
+            <For each={songs().tracks}>
+              {(track, i) =>
+                <li>
+                  <div>{i}</div>
+                  <div>{track.name}</div>
+                </li>
+              }
+            </For>
+          </ol>
+        </Match>
+      </Switch>
     </div>
   );
 };
